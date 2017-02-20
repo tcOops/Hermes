e53d95b65d5384034071bb9d1cb93956b51200f6
rejudgeX <345243921@qq.com>
2017-02-20 17:47:24 +0800

    upd

--- common/src/main/java/io/transwarp/hermes/common/common/Constants.java
+++ common/src/main/java/io/transwarp/hermes/common/common/Constants.java
@@ -6,5 +6,12 @@ package io.transwarp.hermes.common.common;
 
 public class Constants {
     public static final int HBASE_CONN_COUNT = 10;
-    public static final String HBASE_URL = "";
+
+    public static final String ORACLE_DRIVER = "oracle.jdbc.drive.OracleDriver";
+    public static final String ORACLE_URL = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+    public static final String ORACLE_USERNAME = "LOGMINER";
+    public static final String ORACLE_PASSWORD = "LOGMINER";
+    public static final String START_SCN = "2025890";
+    public static final String REDO_LOG_DICTIONARY_FILE_PATH = "/opt/oracle/oradata/orcl/LOGMNR";
+    public static final String REDO_LOG_PATH = "/opt/oracle/oradata/orcl";
 }
